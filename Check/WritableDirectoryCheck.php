6a1866f261c92604fbc11261f84a8022456813e1
Cédric G <c.girard@lexik.fr>
2012-03-27 14:40:06 +0200

    add WritableDirectoryCheck

---
+++ Check/WritableDirectoryCheck.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace SmartTrack\Component\Check;
+
+use Liip\MonitorBundle\Check\Check;
+use Liip\MonitorBundle\Exception\CheckFailedException;
+use Liip\MonitorBundle\Result\CheckResult;
+
+/**
+ * Check if the given directory is writable.
+ *
+ * @author Cédric Girard <c.girard@lexik.fr>
+ */
+class WritableDirectoryCheck extends Check
+{
+    /**
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * Construct.
+     *
+     * @param string $path
+     */
+    public function __construct($path)
+    {
+        $this->path = $path;
+    }
+
+    /**
+     * @see Liip\MonitorBundle\Check.CheckInterface::check()
+     */
+    public function check()
+    {
+        try {
+            $user = exec("whoami");
+
+            if (!is_writable($this->path)) {
+                throw new CheckFailedException(sprintf('The user "%s" is NOT able to write in "%s"', $user, $this->path));
+            }
+
+            $message = sprintf('The user "%s" is able to write in "%s"', $user, $this->path);
+            $result = $this->buildResult($message, CheckResult::SUCCESS);
+
+        } catch (\Exception $e) {
+            $result = $this->buildResult($e->getMessage(), CheckResult::FAILURE);
+        }
+
+        return $result;
+    }
+
+    /**
+     * @see Liip\MonitorBundle\Check.Check::getName()
+     */
+    public function getName()
+    {
+        return 'Writable directory';
+    }
+}
--- HEALTH_CHECKS.md
+++ HEALTH_CHECKS.md
@@ -7,3 +7,7 @@ Checks all entries from `deps` are defined in `deps.lock`.
 ## PhpExtensionsCheck ##
 
 Checks if the extensions specified in the service configuration are enabled in your PHP installation.
+
+## WritableDirectoryCheck ##
+
+Checks if the user executing the script is able to write in the given directoty.
