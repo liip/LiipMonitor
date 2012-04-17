4cbad0b23a7f089cdd587a4d0b662b84291937ec
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2012-04-17 19:59:46 +0200

    Adding a check that checks the version of the website against the latest stable release.

---
+++ Check/SymfonyVersionCheck.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Liip\MonitorExtraBundle\Check;
+
+use \Symfony\Component\HttpKernel\Kernel;
+use Liip\MonitorBundle\Check\Check;
+use Liip\MonitorBundle\Exception\CheckFailedException;
+use Liip\MonitorBundle\Result\CheckResult;
+
+/**
+ * Checks the version of this website against the latest stable release.
+ *
+ * @author Roderik van der Veer <roderik@vanderveer.be>
+ */
+class SymfonyVersionCheck extends Check
+{
+    /**
+     * @var array
+     */
+    protected $directories;
+
+    /**
+     * Construct.
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * @see Liip\MonitorBundle\Check.CheckInterface::check()
+     */
+    public function check()
+    {
+        try {
+            $latestRelease = $this->getLatestSymfonyVersion(); // eg. 2.0.12
+            $currentVersion = Kernel::VERSION;
+            if (version_compare($currentVersion, $latestRelease) >= 0) {
+                $result = $this->buildResult('OK', CheckResult::OK);
+            } else {
+                $result = $this->buildResult('Update to ' . $latestRelease . ' from ' . $currentVersion, CheckResult::CRITICAL);
+            }
+        } catch (\Exception $e) {
+            $result = $this->buildResult($e->getMessage(), CheckResult::CRITICAL);
+        }
+
+        return $result;
+    }
+
+    private function getLatestSymfonyVersion()
+    {
+        $githubUser = 'symfony';
+        $githubRepo = 'symfony';
+
+        // Get GitHub JSON request
+
+        $githubUrl = 'http://github.com/api/v2/json/repos/show/' . $githubUser . '/' . $githubRepo . '/tags';
+        $githubJSONResponse = file_get_contents($githubUrl);
+
+        // Convert it to a PHP object
+
+        $githubResponseArray = json_decode($githubJSONResponse, true);
+        $tagList = array_keys($githubResponseArray["tags"]);
+
+        // Filter out non final tags
+
+        $filteredTagList = array_filter($tagList, function($tag)
+        {
+            return !stripos($tag, "-") && !stripos($tag, "PR") && !stripos($tag, "BETA") && stripos($tag, "v2.0") === 0;
+        });
+
+        // Sort tags
+
+        natcasesort($filteredTagList);
+
+        // The first one is the last stable release for Symfony 2
+
+        $reverseFilteredTagList = array_reverse($filteredTagList);
+        return str_replace("v", "", $reverseFilteredTagList[0]);
+    }
+
+    /**
+     * @see Liip\MonitorBundle\Check.Check::getName()
+     */
+    public function getName()
+    {
+        return 'Symfony version';
+    }
+}
