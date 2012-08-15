3eb2d297387bfa376546dca93ee25567c31fdb36
Michał Dąbrowski <dabrowski@brillante.pl>
2012-08-15 12:41:35 +0200

    Fixing command

--- Check/DiscUsageCheck.php
+++ Check/DiscUsageCheck.php
@@ -17,27 +17,16 @@ class DiscUsageCheck extends Check
     }
 
     public function check()
-    {
-       
-        try{
+    {      
+        $df = disk_free_space($this->path);
+        $dt = disk_total_space($this->path);
+        $du = $dt - $df;
+        $dp = ($du / $dt) * 100;
 
-            $df = disk_free_space($this->path);
-            $dt = disk_total_space($this->path);
-            $du = $dt - $df;
-            $dp = ($du / $dt) * 100;
-
-
-            if($dp >= $this->maximum_disc_usage_in_percent) {
-                throw new CheckFailedException(sprintf('Disc usage is %2d percent. The maximum usage to consider healthy is %2d percentage.', $dp, $this->maximum_disc_usage_in_percent));
-            }
-
-                
-            return $this->buildResult('OK', CheckResult::OK);
-                       
-            
-        } catch (\Exception $e) {
+        if($dp >= $this->maximum_disc_usage_in_percent) {
             return $this->buildResult(sprintf('Disc usage LOW - %s', $e->getMessage()), CheckResult::CRITICAL);
         }
+        return $this->buildResult('OK', CheckResult::OK);
     }
 
     public function getName()
